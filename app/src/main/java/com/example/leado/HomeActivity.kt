5b4846c7ed9b3c9d17ef8d5bc829644ce25b0f93
Aalaa-Abdou <aalaa.m.abdou@gmail.com>
2020-06-06 20:58:28 +0200

    add a NavController and setup the bottomNavBar with it

--- app/src/main/java/com/example/leado/HomeActivity.kt
+++ app/src/main/java/com/example/leado/HomeActivity.kt
@@ -2,45 +2,18 @@ package com.example.leado
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
-
-    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener {item->
-        when(item.itemId){
-            R.id.navigation_home -> {
-                replaceFragment(HomeFragment())
-                true
-            }
-            R.id.navigation_search -> {
-                replaceFragment(SearchFragment())
-                true
-            }
-            R.id.navigation_library -> {
-                replaceFragment(LibraryFragment())
-                true
-            }
-            R.id.navigation_account -> {
-                replaceFragment(AccountFragment())
-                true
-            }
-            else -> false
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        bottom_navigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
-        replaceFragment(HomeFragment())
-
-    }
-    private fun replaceFragment(fragment: Fragment){
-        val fragmentTransaction = supportFragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.frame_layout, fragment).commit()
+        val mainNavController = findNavController(R.id.nav_host_fragment)
+        bottom_navigation_bar.setupWithNavController(mainNavController)
 
     }
 

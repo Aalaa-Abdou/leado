f2082d21f4f237035bb6355eb258384f3d60cd8a
Mariam-MF97 <mariam.mu.farouk@gmail.com>
2020-06-10 20:39:05 +0200

    Create Account Fragment

--- app/src/main/java/com/example/leado/MainActivity.kt
+++ app/src/main/java/com/example/leado/MainActivity.kt
@@ -2,11 +2,53 @@ package com.example.leado
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.leado.models.Mission
+import kotlinx.android.synthetic.main.fragment_account.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val missionList : List<Mission> = getMissionList()
+        mission_recycler.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        mission_recycler.adapter = MissionAdapter(missionList)
+    }
+
+
+    private fun getMissionList(): List<Mission> {
+        val missionList: MutableList<Mission> = mutableListOf()
+        missionList.add(
+            Mission(
+                "Quick Learner",
+                "Complete 1 Course",
+                R.drawable.ic_launcher_background
+            )
+        )
+        missionList.add(
+            Mission(
+                "Master Mind",
+                "Got 1st Place",
+                R.drawable.ic_launcher_background
+            )
+        )
+        missionList.add(
+            Mission(
+                "The achiever",
+                "Logged in everyday",
+                R.drawable.ic_launcher_background
+            )
+        )
+        missionList.add(
+            Mission(
+                "Super Learner",
+                "Complete more than 5 courses",
+                R.drawable.ic_launcher_background
+            )
+        )
+        return missionList
+
     }
 }
